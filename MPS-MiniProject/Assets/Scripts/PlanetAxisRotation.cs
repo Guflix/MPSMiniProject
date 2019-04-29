b194b933bddf4abcb8bb4f3409f813f1ace7fbcd
Dumbthing <47414969+Dumbthing@users.noreply.github.com>
2019-04-29 15:03:03 +0200

    Fixed lighting and new scaling

---
+++ MPS-MiniProject/Assets/Scripts/PlanetAxisRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetAxisRotation : MonoBehaviour
+{
+
+    public float degrPerSec;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, degrPerSec * Time.deltaTime);
+    }
+}

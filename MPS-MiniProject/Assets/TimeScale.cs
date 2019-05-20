fd2d97d0957f5d45f8bacd44266e4b6240157a40
Guflix - Thomas Boelt <thomas@boelthansen.dk>
2019-05-20 11:31:00 +0200

    Time speed + Trails

---
+++ MPS-MiniProject/Assets/TimeScale.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScale : MonoBehaviour
+{
+    public float SpeedOfTime = 1;
+    void FixedUpdate()
+    {
+        Time.timeScale = SpeedOfTime;
+    }
+}

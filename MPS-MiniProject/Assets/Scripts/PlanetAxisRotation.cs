0e5330cb173dbb24db4f0f825135c406933a9233
Guflix - Thomas Boelt <thomas@boelthansen.dk>
2019-05-21 13:14:09 +0200

    Zoom in

--- MPS-MiniProject/Assets/Scripts/PlanetAxisRotation.cs
+++ MPS-MiniProject/Assets/Scripts/PlanetAxisRotation.cs
@@ -6,7 +6,7 @@ public class PlanetAxisRotation : MonoBehaviour
 {
 
     public float daysSpentRotating;
-    public float speed = 1;
+    float speed = 1;
 
 
     // Update is called once per frame

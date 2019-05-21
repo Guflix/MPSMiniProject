6f45e937b7a70b6ad6a6a5810391ee95a1fb8008
Guflix - Thomas Boelt <thomas@boelthansen.dk>
2019-05-21 10:33:43 +0200

    TimeScale slider UI

--- MPS-MiniProject/Assets/Scripts/TimeScale.cs
+++ MPS-MiniProject/Assets/Scripts/TimeScale.cs
@@ -6,6 +6,12 @@ public class TimeScale : MonoBehaviour
 {
     [Range(0.0f,100.0f)]
     public float SpeedOfTime = 1;
+
+   public void AdjustSpeed(float newSpeed)
+    {
+        SpeedOfTime = newSpeed;
+    }
+
     void FixedUpdate()
     {
         Time.timeScale = SpeedOfTime;

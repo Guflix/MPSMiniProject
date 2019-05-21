95352c040f04056528770f8181f7648b5cebd3bf
Jannik15 <jneerd15@student.aau.dk>
2019-05-21 13:16:12 +0200

    Update PlanetAxisRotation.cs

--- MPS-MiniProject/Assets/Scripts/PlanetAxisRotation.cs
+++ MPS-MiniProject/Assets/Scripts/PlanetAxisRotation.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class PlanetAxisRotation : MonoBehaviour
 {
-
     public float daysSpentRotating;
     public float speed = 1;
 
-
-    // Update is called once per frame
     void Update()
     {
-        //transform.RotateAround(Vector3.zero, Vector3.up, degrPerSec * Time.deltaTime);
         transform.Rotate(new Vector3 (0, speed / daysSpentRotating * Time.deltaTime, 0), Space.Self);
-        
     }
 }
